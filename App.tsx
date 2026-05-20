fd1ce03275ce09aefaded9a59db8ecf4898bf8bb
mohdaffankhan <198424854+mohdaffankhan@users.noreply.github.com>
2026-05-20 18:48:34 +0530

    chore: setup react navigation

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Yumly</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: { fontSize: 24, fontWeight: '700', color: '#FF6B35' },
+});
